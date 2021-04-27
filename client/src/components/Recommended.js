afe3404d3f932aa9e123744e49378a518578236e
himil-vasava <himiljack@gmail.com>
2021-04-27 20:24:44 +0530

    Initial client structure

---
+++ client/src/components/Recommended.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Recommended = () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
+
+export default Recommended;
---
+++ client/src/components/Video.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Video = () => {
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+    )
+}
+
+export default Video;
