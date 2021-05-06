208d97f1b7d8eb00bf7766f86af9a22ff579e3d2
himil-vasava <himiljack@gmail.com>
2021-05-06 12:14:20 +0530

    Basic upload structure

--- client/src/components/Upload/Upload.js
+++ client/src/components/Upload/Upload.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Upload.css';
 
 const Upload = () => {
     return (
-        <div>
-            <form>
-            </form>
+        <div className="upload">
+            <div className="form-style-5">
+                <form>
+                    <fieldset>
+                        <input type="text" name="title" placeholder="Title" />
+                        <textarea name="description" placeholder="Description" />
+
+                        <input type="button" value="Select"/>
+                        <input type="submit" />
+                    </fieldset>
+                </form>
+            </div>
         </div>
     )
 }
