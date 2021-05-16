235ed0fdd1aa5a4fd0ba0002b98eb9b027ee54b4
kanhaiya <kumarkhg4@gmail.com>
2021-05-16 18:57:39 +0530

    Prefinal version

--- client/src/components/VideoInfo/Comments.js
+++ client/src/components/VideoInfo/Comments.js
@@ -1,6 +1,7 @@
 import { Avatar } from "@material-ui/core";
 import styled from "styled-components";
 import React,{useState} from "react";
+import Connect from "../../utils";
 
 const CommentWrapper = styled.div`
   .addcomment{
@@ -26,6 +27,14 @@ const CommentComp = ({comment})=>{
 
 const Comments = ({video})=>{
     const [myComment,setMyComment] = useState("");
+    const [comments,setComments] = useState(video.comments);
+    const addmyComment = (e)=>{
+        if(myComment&&e.keyCode===13){
+            Connect(`/video/${video._id}/comments`,{body:{text:myComment}}).then(data=>{
+                setComments(data.comment,...comments);
+            })
+        }
+    }
     return <CommentWrapper>
         <h3>{video.comments.length} comments</h3>
         <div className="addcomment">
@@ -39,13 +48,14 @@ const Comments = ({video})=>{
                     name="comment"
                     value={myComment}
                     placeholder="Add a comment"
+                    onKeyDown={addmyComment}
                     onChange={(e)=>setMyComment(e.target.value)}
                 />
                     
             </div>
             <hr/>
             <div className="allcomment">
-                {video.comments.map(function(x){
+                {comments.map(function(x){
                     return <CommentComp comment={x} key={x._id}/>
                 })}
             </div>
